bac0e60091481d8068eb4e0368778ced8f5f8f76
Matthew Plant <maplant@protonmail.com>
2024-01-24 22:33:33 -0500

    Add redirect for gc.html

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,15 @@
-use axum::Router;
+use axum::{response::Redirect, Router};
 use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
+        .route(
+            "/gc.html",
+            get(|| async {
+                Redirect::permanent("/2020-04-25-Writing-a-Simple-Garbage-Collector-in-C.html")
+            }),
+        )
         .fallback_service(ServeDir::new(".").not_found_service(ServeFile::new("notfound.html")));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
